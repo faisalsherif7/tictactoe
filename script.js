e8156fafaa0cd731dcb0eef66389604ff94b43da
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-10-19 23:10:46 +0530

    Move entire code into a single gameBoard module

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Create gameBoard module
 const gameBoard = (function() {
     let array = ['X', 'O', 'X', 'O', 'O', 'X', 'O', 'X', 'O'];
-    return array;
+    
+    const board = document.querySelector('.board')
+    for (let i = 0; i < 9; i++) {
+        board.innerHTML += `
+        <div class="box">
+        <p>${array[i]}</p>
+        </div>
+        `;
+    }
 })()
-
-const board = document.querySelector('.board')
-for (let i = 0; i < 9; i++) {
-    board.innerHTML += `
-    <div class="box">
-    <p>${gameBoard[i]}</p>
-    </div>
-    `;
-}
