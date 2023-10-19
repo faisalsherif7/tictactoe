4b14359667978863dfb60064f65a8004b8e97e31
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-10-20 00:13:59 +0530

    Implement a purely javascript version of displaying the array

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const gameBoard = (function() {
+const Gameboard = (function() {
     let array = ['X', 'O', 'X', 'O', 'O', 'X', 'O', 'X', 'O'];
     
     const board = document.querySelector('.board')
-    for (let i = 0; i < 9; i++) {
-        board.innerHTML += `
-        <div class="box">
-        <p>${array[i]}</p>
-        </div>
-        `;
+
+    let counter = 0
+    for (let i = 0; i < 3; i++) {
+        board.textContent += array[counter]
+        board.textContent += array[counter + 1]
+        board.textContent += array[counter + 2]
+        board.textContent += '\r\n'
+        counter = counter + 3
     }
 })()
