28441d12598e83bafd60b4ace5034658935a452d
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-10-20 15:21:55 +0530

    Move displayPopulate() to inside gameBoard module and rename it

--- script.js
+++ script.js
@@ -12,35 +12,35 @@ const gameBoard = (function() {
         }
     }
 
-    return {
-        arr,
-    };
-
-})();
-
-
-const displayPopulate = (function() {
-    const arr = gameBoard.arr;
-    console.log(arr)
-    const board = document.querySelector('.board')
-
-    let table = document.createElement('table');
-    let tableBody = document.createElement('tbody');
-
-    for (let i = 0; i < 3; i++) {
-        let row = document.createElement('tr');
-        for (let j = 0; j < 3; j++) {
-            let marker = arr[i][j];
-            const cell = document.createElement("td");
-            const cellText = document.createTextNode(marker);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-            cell.setAttribute('data-i', `${i}`);
-            cell.setAttribute('data-j', `${j}`)
+    const updateBoard = (function() {
+        const board = document.querySelector('.board')
+    
+        let table = document.createElement('table');
+        let tableBody = document.createElement('tbody');
+    
+        for (let i = 0; i < 3; i++) {
+            let row = document.createElement('tr');
+            for (let j = 0; j < 3; j++) {
+                let marker = arr[i][j];
+                const cell = document.createElement("td");
+                const cellText = document.createTextNode(marker);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                cell.setAttribute('data-i', `${i}`);
+                cell.setAttribute('data-j', `${j}`)
+            }
+            tableBody.appendChild(row);
         }
-        tableBody.appendChild(row);
-    }
-    table.appendChild(tableBody);
-    board.appendChild(table);
+        table.appendChild(tableBody);
+        board.appendChild(table);
+    })();
 
 })();
+
+function createPlayer (name, marker) {
+    const mark = function () {
+        
+    }
+    return { name, marker, mark };
+}
+
