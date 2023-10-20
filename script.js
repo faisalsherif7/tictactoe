aa5ba5609f61d82a3e888c6b15dd460b6a4583b9
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-10-20 12:01:34 +0530

    Create function to populate display with current array

--- script.js
+++ script.js
@@ -12,6 +12,33 @@ const gameBoard = (function() {
         }
     }
 
-    console.log(arr);
+    return {
+        arr,
+    };
+
+})();
+
+
+const displayPopulate = (function() {
+    const arr = gameBoard.arr;
+    console.log(arr)
+    const board = document.querySelector('.board')
+
+    let table = document.createElement('table');
+    let tableBody = document.createElement('tbody');
+
+    for (let i = 0; i < 3; i++) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < 3; j++) {
+            let marker = arr[i][j];
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(marker);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        tableBody.appendChild(row);
+    }
+    table.appendChild(tableBody);
+    board.appendChild(table);
 
 })();
