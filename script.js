73c5af8aac9c699364713ae1d2ce0a578da89e86
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-10-20 15:48:49 +0530

    Create dummy players, gameController() module and switchTurns() function

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const gameBoard = (function() {
-    
-    // Create array to store gameboard
     let arr = [];
     let rows = 3;
     let columns = 3;
 
+    // Populate array with X's as default
     for (let i = 0; i < rows; i++) {
         arr[i] = [];
         for (let j = 0; j < columns; j++) {
@@ -12,7 +11,8 @@ const gameBoard = (function() {
         }
     }
 
-    const updateBoard = (function() {
+    // Display array as an html table
+    const displayBoard = function() {
         const board = document.querySelector('.board')
     
         let table = document.createElement('table');
@@ -33,14 +33,50 @@ const gameBoard = (function() {
         }
         table.appendChild(tableBody);
         board.appendChild(table);
-    })();
+    };
 
+    const updateBoard = () => {
+
+    }
+
+    return {
+        displayBoard,
+        updateBoard
+    }
 })();
 
 function createPlayer (name, marker) {
-    const mark = function () {
-        
-    }
-    return { name, marker, mark };
-}
+    return { name, marker };
+};
 
+const player1 = createPlayer('player1', 'X');
+const player2 = createPlayer('player2', 'O');
+
+
+
+const gameController = (function() {
+    let currentPlayer = player1;
+
+    const switchTurns = () => {
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+    let allCells = document.querySelectorAll('td')
+    allCells.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            const i = cell.dataset.i;
+            const j = cell.dataset.j;
+            updateBoard(i, j, currentPlayer.marker)
+        })
+    });
+
+    return {
+        currentPlayer, 
+    }
+})();
+
+console.log(gameController.currentPlayer.marker)
